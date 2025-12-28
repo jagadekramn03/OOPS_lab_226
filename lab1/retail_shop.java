059a47caa0f1562f15a874478856aa5d8f737cbe
jagadekramn03 <jagadekramroy.22@gmail.com>
2025-12-28 16:07:42 +0530

    Add files via upload

---
+++ lab1/retail_shop.java
@@ -0,0 +1,7 @@
+public class retail_shop {
+         public static void main(String[] args) {
+              int invoice_id,product_id,quan;
+              double cost,discount,price;
+              Boolean feedback;
+        }
+}

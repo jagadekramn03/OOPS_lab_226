86fee6ab82a730f9ede74f4f128559b8ff32b3f0
jagadekramn03 <jagadekramroy.22@gmail.com>
2025-12-28 16:38:33 +0530

    Add files via upload

---
+++ lab1/retail_shop.java
@@ -0,0 +1,10 @@
+public class retail_shop {
+          public static void main(String[] args) {
+               int thingr=730,things=220,thingj=500;
+               double price;
+               price =((thingr*2)-(things)+(thingj*3));
+               price=price-(.1*price);
+               price=price+(0.05*price);
+               System.out.println(price);
+}
+}

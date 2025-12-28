89ff5aff0ecd3cc9d3b5664d03996bb797293e7b
jagadekramn03 <jagadekramroy.22@gmail.com>
2025-12-28 16:20:18 +0530

    Delete lab1/retail_shop.java

--- lab1/retail_shop.java
+++
@@ -1,7 +0,0 @@
-public class retail_shop {
-         public static void main(String[] args) {
-              int invoice_id,product_id,quan;
-              double cost,discount,price;
-              Boolean feedback;
-        }
-}

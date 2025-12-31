972ed5cb5a3e93097722e911eebac3aa910c7d33
jagadekramn03 <jagadekramroy.22@gmail.com>
2025-12-31 16:30:55 +0530

    Add files via upload

---
+++ lab1/Operations.java
@@ -0,0 +1,36 @@
+public class Operations {
+public static void main(String[] agrs)
+{
+int a=73 , b=22;
+int s=a+b;
+int d=a-b;
+int p=a*b;
+float q=a/b;
+int r=a%b;
+boolean s1=a==b;
+boolean s2=a>=b;
+boolean s3=a<b;
+boolean s4=a!=b;
+boolean s5=(a>b)&&(a!=0);
+boolean s6=(a==b)||(b<=a);
+boolean s7=!(a!=b);
+int s8=a++;
+int s9=b--;
+System.out.println(s);
+System.out.println(d);
+System.out.println(p);
+System.out.println(q);
+System.out.println(r);
+System.out.println(s1);
+System.out.println(s2);
+System.out.println(s3);
+System.out.println(s4);
+System.out.println(s5);
+System.out.println(s6);
+System.out.println(s7);
+System.out.println(s8);
+System.out.println(s9);
+}
+}
+
+

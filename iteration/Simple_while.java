75e8f588b3ba6ab0ebd161773c7d164a23ecaaa1
jagadekramn03 <jagadekramroy.22@gmail.com>
2026-01-24 18:16:45 +0530

    Add files via upload

---
+++ iteration/Simple_while.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Simple_while
+{
+public static void main(String[] agrs)
+{
+Scanner a=new Scanner(System.in);
+System.out.println("Enter the number for counting till that ");
+int b=a.nextInt();
+int i=1;
+while(i<=b)
+{
+System.out.println(i);
+i++;
+}
+}
+}
+
